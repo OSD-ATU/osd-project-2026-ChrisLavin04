0a977012612fc9defab9f7832c54a9a7b57d0fec
ChrisLavin04 <S00251315@atu.ie>
2026-03-18 21:39:56 +0000

    Updated EC2 apiUrl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,5 @@ export const environment = {
 //  apiUrl: 'http://localhost:3000'
 
 // EC2 API URL
-  apiUrl: 'http://ec2-54-246-252-155.eu-west-1.compute.amazonaws.com:3000'
+  apiUrl: 'http://ec2-52-16-180-77.eu-west-1.compute.amazonaws.com:3000'
 };
