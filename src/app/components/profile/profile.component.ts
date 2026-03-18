aba320f5c3eb20971a165d9a6a0f391fa4089a16
ChrisLavin04 <S00251315@atu.ie>
2026-03-18 19:26:30 +0000

    Add "teams you coach" section to profile with details and styling

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../models/user.model';
+import { TeamService } from '../../services/team.service';
+import { Team } from '../../models/team.model';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit {
   user: User | null = null;
+  coachedTeams: Team[] = [];
 
-  constructor(public authService: AuthService, private router: Router) {}
+  constructor(
+    public authService: AuthService,
+    private router: Router,
+    private teamService: TeamService
+  ) {}
 
   ngOnInit(): void {
-    this.authService.currentUser$.subscribe(u => this.user = u);
+    this.authService.currentUser$.subscribe(u => {
+      this.user = u;
+      if (u && u._id) {
+        this.teamService.getTeams().subscribe(teams => {
+          this.coachedTeams = teams.filter(team => team.coach === u._id);
+        });
+      }
+    });
   }
 
   goToEditProfile() {
