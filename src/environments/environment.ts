c9a3490d04b4147d59fc058345ce1ba834343f0c
ChrisLavin04 <S00251315@atu.ie>
2026-03-18 20:52:31 +0000

    Added EC2 URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000'
+// Local API URL
+//  apiUrl: 'http://localhost:3000'
+
+// EC2 API URL
+  apiUrl: 'http://ec2-54-246-252-155.eu-west-1.compute.amazonaws.com:3000'
 };
