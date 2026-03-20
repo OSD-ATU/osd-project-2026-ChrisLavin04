9142cfeeb1e8dfe6e135f3dbe939b2acbd792f37
ChrisLavin04 <S00251315@atu.ie>
2026-03-20 23:38:47 +0000

    Updated apiUrl in environment.ts to use HTTPS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
 //  apiUrl: 'http://localhost:3000'
 
 // EC2 API URL. Elastic IP: 54.228.193.188
-  apiUrl: 'http://ec2-54-228-193-188.eu-west-1.compute.amazonaws.com:3000',
+  apiUrl: 'https://ec2-54-228-193-188.eu-west-1.compute.amazonaws.com:3000',
   // AWS Lambda API URL for standings calculation
   standingsApiUrl: 'https://po703b7766.execute-api.eu-west-1.amazonaws.com/calculate-standings'
 
