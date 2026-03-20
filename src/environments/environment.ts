3cae628ffa8eb0be3beb048342c6b316ccc34c07
ChrisLavin04 <S00251315@atu.ie>
2026-03-20 12:24:41 +0000

    edited IP in environment.ts to Elastic IP for EC2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
 // Local API URL
 //  apiUrl: 'http://localhost:3000'
 
-// EC2 API URL
-  apiUrl: 'http://ec2-52-16-180-77.eu-west-1.compute.amazonaws.com:3000'
+// EC2 API URL. Elastic IP: 54.228.193.188
+  apiUrl: 'http://ec2-54-228-193-188.eu-west-1.compute.amazonaws.com:3000'
 };
